479ab0633f2d765c3f7a79080b6ef5e2da9b3833
m.vanduijn <m.vanduijn@zefiros.eu>
2018-05-02 16:10:13 +0200

    Improved hyperthread detection

--- sync/include/sync/util/threads.h
+++ sync/include/sync/util/threads.h
@@ -27,36 +27,156 @@
 #ifndef __SYNCLIB_THREADS_H__
 #define __SYNCLIB_THREADS_H__
 
+#include "preproc/preproc.h"
+
 #include <future>
-#ifdef _WIN32
+#ifdef IS_WINDOWS
 #include <windows.h>
 #endif
 
+#include <set>
+#include <thread>
+
 namespace SyncLib
 {
-    namespace Internal
+    namespace Util
     {
-        inline void PinThread(size_t s)
+
+        // Taken from boost
+        inline unsigned PhysicalConcurrency() noexcept
         {
-            int maxS = static_cast<int>(std::thread::hardware_concurrency());
-            int core = static_cast<int>(s) % maxS;
-#ifdef _WIN32
-            DWORD_PTR mask = 1;
-            mask = mask << core;
-            SetThreadAffinityMask(GetCurrentThread(), mask);
-            SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
-#endif // _WIN32
+#if defined(IS_WINDOWS)
+            unsigned cores = 0;
+            DWORD size = 0;
 
-#if defined(__GNUC__) && !defined(__APPLE__)
-            cpu_set_t cpuset;
-            CPU_ZERO(&cpuset);
-            CPU_SET(core, &cpuset);
+            GetLogicalProcessorInformation(nullptr, &size);
 
-            pthread_t currentThread = pthread_self();
-            pthread_setaffinity_np(currentThread, sizeof(cpu_set_t), &cpuset);
+            if (ERROR_INSUFFICIENT_BUFFER != GetLastError())
+            {
+                return 0;
+            }
+
+            std::vector<SYSTEM_LOGICAL_PROCESSOR_INFORMATION> buffer(size);
+
+            if (GetLogicalProcessorInformation(&buffer.front(), &size) == FALSE)
+            {
+                return 0;
+            }
+
+            const size_t elements = size / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION);
+
+            for (size_t i = 0; i < elements; ++i)
+            {
+                if (buffer[i].Relationship == RelationProcessorCore)
+                {
+                    ++cores;
+                }
+            }
+
+            return cores;
+#elif defined(IS_LINUX)
+
+            try
+            {
+                using namespace std;
+
+                ifstream proc_cpuinfo("/proc/cpuinfo");
+
+                const string_view physical_id("physical id"), core_id("core id");
+
+                typedef std::pair<unsigned, unsigned> core_entry; // [physical ID, core id]
+
+                std::set<core_entry> cores;
+
+                core_entry current_core_entry;
+
+                string line;
+
+                while (getline(proc_cpuinfo, line))
+                {
+                    if (line.empty())
+                    {
+                        continue;
+                    }
+
+                    vector<string_view> key_val(::SyncLib::Util::Split(line, ':'));
+                    // boost::split(key_val, line, boost::is_any_of(":"));
+
+                    if (key_val.size() != 2)
+                        // return hardware_concurrency();
+                    {
+                        continue;
+                    }
+
+                    // string key = key_val[0];
+                    // string value = key_val[1];
+                    // boost::trim(key);
+                    // boost::trim(value);
+                    string key(::SyncLib::Util::Trim(key_val[0]));
+                    string value(::SyncLib::Util::Trim(key_val[1]));
+
+                    if (key == physical_id)
+                    {
+                        // current_core_entry.first = boost::lexical_cast<unsigned>(value);
+                        current_core_entry.first = stoi(value);
+                        continue;
+                    }
+
+                    if (key == core_id)
+                    {
+                        // current_core_entry.second = boost::lexical_cast<unsigned>(value);
+                        current_core_entry.second = stoi(value);
+                        cores.insert(current_core_entry);
+                        continue;
+                    }
+                }
+
+                // Fall back to hardware_concurrency() in case
+                // /proc/cpuinfo is formatted differently than we expect.
+                // return cores.size() != 0 ? cores.size() : hardware_concurrency();
+                return cores.size() != 0 ? cores.size() : std::thread::hardware_concurrency();
+            }
+            catch (...)
+            {
+                // return hardware_concurrency();
+                return std::thread::hardware_concurrency();
+            }
+
+#elif defined(IS_MACOS)
+            int count;
+            size_t size = sizeof(count);
+            return sysctlbyname("hw.physicalcpu", &count, &size, NULL, 0) ? 0 : count;
+#else
+            return std::thread::hardware_concurrency();
 #endif
         }
+    } // namespace Util
+} // namespace SyncLib
+
+namespace SyncLibInternal
+{
+    inline void PinThread(const size_t s)
+    {
+        const int maxS = static_cast<int>(std::thread::hardware_concurrency());
+        const int virtiualConcurrency = maxS / SyncLib::Util::PhysicalConcurrency();
+        int core = static_cast<int>(s) % maxS;
+        core *= virtiualConcurrency;
+        core = (core % maxS) + (core / maxS);
+#if defined(IS_WINDOWS)
+
+        DWORD_PTR mask = 1;
+        mask = mask << core;
+        SetThreadAffinityMask(GetCurrentThread(), mask);
+        SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+#elif defined(IS_LINUX)
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(core, &cpuset);
+
+        pthread_t currentThread = pthread_self();
+        pthread_setaffinity_np(currentThread, sizeof(cpu_set_t), &cpuset);
+#endif
     }
-}
+} // namespace SyncLibInternal
 
 #endif
